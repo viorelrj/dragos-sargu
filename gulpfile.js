039bd88596c38f60424ead69c779c162dac6f5e8
Viorel RInja <viorelrinja@gmail.com>
2018-12-21 10:34:40 +0200

    Add gulp prod task

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ var notify = require("gulp-notify");
 var browserSync = require("browser-sync");
 var sass = require("gulp-sass");
 var autoprefixer = require("gulp-autoprefixer");
-
+var cleanCSS = require("gulp-clean-css");
 
 var baseFolder = "src/";
 
@@ -13,16 +13,34 @@ var baseFolder = "src/";
 
 gulp.task("styles", function() {
 	gulp.src("src/scss/**/*.scss")
-	.pipe(sass({ outputStyle: 'expanded' }).on("error", notify.onError()))
+	.pipe(sass({ outputStyle: "expanded" }).on("error", notify.onError()))
 	.pipe(autoprefixer(["last 15 versions"]))
 	.pipe(gulp.dest("src/css"))
 	.pipe(browserSync.stream())
 });
 
-gulp.task('browser-sync', function() {
+gulp.task("minify:css", function() {
+	gulp.start("styles");
+	gulp.src(baseFolder +"css/*.css")
+	.pipe(cleanCSS())
+	.pipe(gulp.dest("prod/css"))
+});
+
+gulp.task("prod", function(){
+	gulp.start("minify:css");
+
+	gulp.src(baseFolder + "*.html")
+	.pipe(gulp.dest("prod"));
+
+	gulp.src(baseFolder + "img/**/*", {base: baseFolder})
+	.pipe(gulp.dest("prod"))
+})
+
+
+gulp.task("browser-sync", function() {
 	browserSync({
 		server: {
-			baseDir: 'src'
+			baseDir: "src"
 		},
 		notify: true,
 		open: true,
@@ -32,8 +50,8 @@ gulp.task('browser-sync', function() {
 });
 
 gulp.task("watch", ["styles", "browser-sync"], function() {
-	gulp.watch("src/scss/**/*.scss", ['styles']);
-	gulp.watch('src/**/*.html', browserSync.reload);
+	gulp.watch("src/scss/**/*.scss", ["styles"]);
+	gulp.watch("src/**/*.html", browserSync.reload);
 })
 
 gulp.task("default", ["watch"])
